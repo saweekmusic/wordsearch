f645555a0b6d0e87a5ad57d773a3e36933ad2f3a
SAWEEK <saweek.official@gmail.com>
2025-04-21 23:05:53 +0200

    Restructured project for pip installation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="wordsearch",  # ✅ Your PyPI package name
+    name="wordsearch-gen",  # ✅ Your PyPI package name
     version="0.1.0",
     author="Sviatoslav Trofymenko",
     author_email="sviatoslavtrofymenko@icloud.com",  # Replace with your email
